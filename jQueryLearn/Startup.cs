967a9892ca8d99785dd6be43bcf9811fc3a45771
nhsonet <engmunacse@gmail.com>
2015-07-07 11:14:35 +0600

    First commit

---
+++ jQueryLearn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jQueryLearn.Startup))]
+namespace jQueryLearn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
